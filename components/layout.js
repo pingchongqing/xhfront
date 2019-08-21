61d70be45d8a7a095fc4b48e42072c20e6ebcd8c
66864519@qq.com <66864519@qq.com>
2019-08-21 23:02:09 +0800

    yy

--- components/layout.js
+++ components/layout.js
@@ -5,8 +5,13 @@ import Head from 'next/head'
 const Layout = (props) => (
   <div>
     <Head>        
-      <title>江西安邦工程质量检测有限公司</title>
+      <title>江西兴辉水利水电工程有限公司</title>
     </Head>
+    <div className="topnav">
+      <div className="w1200 toptext">
+        <span>客服热线：18720958187（8:30-17:30）</span>
+      </div>      
+    </div>
     <div className="header">
       <div className="logo">
         <img src="/static/images/logo.jpg" alt="江西安邦logo" />
@@ -14,7 +19,17 @@ const Layout = (props) => (
       <div className="nav">
         <ul>
           <li><a href="/">首页</a></li>
-          <li><a href="/about">公司简介</a></li>
+          <li>
+            <a href="/about">企业概况</a>
+            <div className="submenu">
+              <span><a href="protection.html">企业简介</a></span>
+              <span><a href="protection.html?id=99">企业资质</a></span>
+              <span><a href="protection.html?id=102">企业荣誉</a></span>
+              <span><a href="protection.html?id=102">企业文化</a></span>
+              <span><a href="protection.html?id=102">组织结构</a></span>
+            </div>
+          </li>
+          <li><a href="/about">工程业绩</a></li>
           <li><a href="/news">新闻动态</a></li>
           <li><a href="/contact">联系我们</a></li>
         </ul>
@@ -23,7 +38,7 @@ const Layout = (props) => (
     { props.children }
     <div className="bg-515151">
       <div className="w1200 bottom">
-        <span>COPYRIGHT ©2019江西安邦工程质量检测有限公司 All right reserved 赣ICP备 05xx0号</span>
+        <span>COPYRIGHT 江西兴辉水利水电工程有限公司 All right reserved 赣ICP备 05xx0号</span>
       </div>
     </div>
   </div>
